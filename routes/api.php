a6d4f15d2c152ac536d998dc63aef4964b66e36e
salmia <salmiaali16@gmail.com>
2026-06-03 01:22:00 +0800

    Menyelesaikan modul autentikasi sanctum dan middleware role

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'OK'
-    ]);
+Route::middleware('auth:sanctum')->group(function () {
+    // Rute Category tanpa fungsi destroy bawaan
+    Route::apiResource('categories', CategoryController::class)->except(['destroy']);
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->middleware('role:admin');
+
+    // Rute Item tanpa fungsi destroy bawaan
+    Route::apiResource('items', ItemController::class)->except(['destroy']);
+    Route::delete('items/{item}', [ItemController::class, 'destroy'])->middleware('role:admin');
 });
-
-Route::apiResource('items', ItemController::class);
-Route::apiResource('categories', CategoryController::class);
