8e967154c3ce7fcff0616b1f7bfb492b730efd56
salmia <salmiaali16@gmail.com>
2026-06-21 23:20:23 +0800

    feat: implement item filter and add api documentation

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -2,24 +2,25 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCategoryRequest extends FormRequest {
+class StoreCategoryRequest extends FormRequest
+{
+    public function authorize() { return true; }
 
-    public function authorize() {
-        return true;
+    protected function prepareForValidation()
+    {
+        $input = $this->all();
+        array_walk($input, function (&$val) {
+            if (is_string($val)) $val = trim(strip_tags($val));
+        });
+        $this->merge($input);
     }
 
-    public function rules() {
+    public function rules()
+    {
         return [
-            'name' => 'required|string|unique:categories,name'
-        ];
-    }
-
-    public function messages() {
-        return [
-            'name.unique' => 'Nama kategori sudah ada.'
+            "name" => "required|string|max:255|unique:categories,name",
         ];
     }
 }
